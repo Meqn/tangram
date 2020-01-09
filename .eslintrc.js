f187850bebda997779d75f78fc778eacc3d8ae1f
Mervin <mengqing723@gmail.com>
2020-01-09 14:43:30 +0800

    :rotating_light: 移除部分eslint警告限制

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 0,
+    'comma-dangle': 0,
     'no-trailing-spaces': 0,
     'space-before-function-paren': 0,
   },
