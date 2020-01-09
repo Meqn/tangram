f6f61a3ca0b37619c5b6b6b65dbbc037821c6571
Mervin <mengqing723@gmail.com>
2020-01-09 14:42:36 +0800

    :wrench: 配置vue完整包别名

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
+const path = require('path')
+
+const resolve = (dir) => path.join(__dirname, dir)
 
 module.exports = {
   parallel: require('os').cpus().length > 1,
   devServer: {
     port: 3600
+  },
+  chainWebpack (config) {
+    config.resolve.alias
+      .set('vue$', 'vue/dist/vue.esm.js')
+      .set('@', resolve('src'))
   }
 }
