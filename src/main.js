e31587da5498b1bfc2d85417f09de8182850fc69
Mervin <mengqing723@gmail.com>
2020-01-20 18:08:34 +0800

    :new: 全局安装用于测试的组件

--- src/main.js
+++ src/main.js
@@ -6,10 +6,13 @@ import store from './store'
 // 引入 element-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
 // 引入 自定义样式
+import Tangram from './packages'
 import '@/styles/index.scss'
 
 Vue.use(ElementUI)
+Vue.use(Tangram)
 
 Vue.config.productionTip = false
 
