dc9f6c81f07adf833eca236cabad9e4695bbd7a9
Mervin <mengqing723@gmail.com>
2020-02-20 20:23:46 +0800

    :zap: 合并组件及其配置文件导出

--- src/packages/index.js
+++ src/packages/index.js
@@ -1,18 +1,27 @@
-// 通用组件
-import Header from './header'
-import Text from './text'
+// 通用
+import Header, { headerConfig } from './header'
+import Text, { textConfig } from './text'
 
-// 布局组件
-import Row from './row'
-import Col from './col'
+// 布局
+// 一行两列、一行三列、 一行四列、一行五列、一行六列、 两行一列、三行一列、四行一列、五行一列
+// grid: 四宫格、九宫格、六宫格
+import Row, { rowConfig } from './row'
+import Col, { colConfig } from './col'
 
-// 设备组件
-import Environment from './environment'
-import Staff from './staff'
-import TowerCrane from './towerCrane'
-import Elevator from './elevator'
-import MonitorSlide from './monitorSlide'
+// 设备
+import Environment, { environmentConfig } from './environment'
+import Staff, { staffConfig } from './staff'
+import TowerCrane, { towerCraneConfig } from './towerCrane'
+import Elevator, { elevatorConfig } from './elevator'
+import MonitorSlide, { monitorSlideConfig } from './monitorSlide'
 
+// 图表
+
+// 其他
+
+/************************************************************************************
+ * 组件部分
+ */
 const components = [
   Header,
   Text,
@@ -34,6 +43,54 @@ const install = function(Vue) {
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue)
 }
+
+/************************************************************************************
+ * 组件包配置部分
+ */
+export const packages = [
+  {
+    title: '通用',
+    name: 'common',
+    icon: 'files',
+    components: [
+      headerConfig,
+      textConfig
+    ]
+  },
+  {
+    title: '布局',
+    name: 'layout',
+    icon: 'news',
+    components: [
+      rowConfig,
+      colConfig
+    ]
+  },
+  {
+    title: '设备',
+    name: 'device',
+    icon: 'cpu',
+    components: [
+      environmentConfig,
+      staffConfig,
+      towerCraneConfig,
+      elevatorConfig,
+      monitorSlideConfig
+    ]
+  },
+  {
+    title: '图表',
+    name: 'chart',
+    icon: 'pie-chart',
+    components: []
+  },
+  {
+    title: '其他',
+    name: 'more',
+    icon: 'set-up',
+    components: []
+  }
+]
 
 export default {
   install,
